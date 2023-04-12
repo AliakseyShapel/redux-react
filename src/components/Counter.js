dd68875e2073d4330ef76d64c915e35ae1e7ff79
AliakseyShapel <53071397+AliakseyShapel@users.noreply.github.com>
2023-04-12 16:32:17 +0300

    239. Working with Multiple State Properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,7 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
 
   function incrementHandler(value) {
     dispatch({ type: 'INCREMENT', value: value });
@@ -12,13 +13,14 @@ const Counter = () => {
   function decrementHandler(value) {
     dispatch({ type: 'DECREMENT', value: value });
   }
-
-  function toggleCounterHandler() {}
+  function toggleCounterHandler() {
+    dispatch({ type: 'TOGGLE' });
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler.bind(null, 1)}>Increment</button>
         <button onClick={incrementHandler.bind(null, 5)}>Increment by 5</button>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,15 @@ import { legacy_createStore } from 'redux';
 
 function counterReducer(state, action) {
   if (action.type === 'INCREMENT') {
-    return { counter: state.counter + action.value };
+    return { ...state, counter: state.counter + action.value };
   }
   if (action.type === 'DECREMENT') {
-    return { counter: state.counter - action.value };
+    return { ...state, counter: state.counter - action.value };
   }
-  return { counter: 0 };
+  if (action.type === 'TOGGLE') {
+    return { ...state, showCounter: !state.showCounter };
+  }
+  return { counter: 0, showCounter: true };
 }
 
 const store = legacy_createStore(counterReducer);
